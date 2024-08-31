70d61e3a3f9276db1eb1fe70eacce1fa17de91cc
Librariann <okpc0306@naver.com>
2024-08-31 10:00:00 +0900

    create auth store with zustand

---
+++ app/store/authStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+
+interface User {
+  username: string;
+  email: string;
+}
+
+interface AuthState {
+  accessToken: string | null;
+  user: User | null;
+  isAuthenticated: boolean;
+  setAuth: (accessToken: string, user: User) => void;
+  clearAuth: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  accessToken: null,
+  user: null,
+  isAuthenticated: false,
+  setAuth: (accessToken, user) =>
+    set({ accessToken, user, isAuthenticated: true }),
+  clearAuth: () =>
+    set({ accessToken: null, user: null, isAuthenticated: false }),
+}));
